89008a3f57968bddcbc15097b9242679781bb619
lihaojie <lihaojie.wy@gmail.com>
2026-05-19 07:53:42 +0800

    model.py

---
+++ src/model.py
@@ -0,0 +1,8 @@
+from torch.nn import nn
+class ReviewAnalyseModel(nn.Module):
+    def __init__(self):
+        self.embedding = nn.Embedding()
+
+        pass
+    def forward(self,x):
+        pass
